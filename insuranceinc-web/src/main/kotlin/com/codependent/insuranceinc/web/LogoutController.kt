98be4ae774799264d76c77a77c12a01a758cf89e
Jose Antonio Iñigo Garrido <jainigo@profile.es>
2019-04-09 15:37:28 +0200

    fix

--- insuranceinc-web/src/main/kotlin/com/codependent/insuranceinc/web/LogoutController.kt
+++ insuranceinc-web/src/main/kotlin/com/codependent/insuranceinc/web/LogoutController.kt
@@ -1,13 +1,9 @@
 package com.codependent.insuranceinc.web
 
-import com.codependent.insuranceinc.dto.Credentials
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.ModelAttribute
-import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.server.ServerWebExchange
 import reactor.core.publisher.Mono
-import reactor.core.publisher.toMono
 
 @Controller
 class LogoutController {
@@ -17,14 +13,6 @@ class LogoutController {
         return swe.session.map {
             it.invalidate()
             "redirect:/login"
-        }
-    }
-
-    @PostMapping("/login")
-    fun doLogin(@ModelAttribute credentials: Credentials, swe: ServerWebExchange): Mono<String> {
-        return swe.session.map {
-            it.attributes["userId"] = credentials.userId
-            "redirect:/globalPosition"
         }
     }
 
