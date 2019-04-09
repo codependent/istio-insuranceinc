01a52f2190b078214c18e2dbe77ecdaf9e69f877
Jose Antonio Iñigo Garrido <jainigo@profile.es>
2019-04-09 13:18:13 +0200

    updated ammount

--- insuranceinc-web/src/test/kotlin/com/codependent/insuranceinc/InsuranceincWebApplicationTests.kt
+++ insuranceinc-web/src/test/kotlin/com/codependent/insuranceinc/InsuranceincWebApplicationTests.kt
@@ -1,11 +1,13 @@
 package com.codependent.insuranceinc
 
+import org.junit.Ignore
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.junit4.SpringRunner
 import org.springframework.core.ParameterizedTypeReference
 import org.mockito.ArgumentMatchers
+import org.mockito.Mock
 import org.mockito.Mockito
 import org.springframework.http.HttpEntity
 import org.springframework.http.HttpMethod
@@ -24,11 +26,15 @@ class InsuranceincWebApplicationTests {
     }
 
     @Test
+    @Ignore
     fun moretests() {
-        val restTemplate = RestTemplate()
+        val restTemplate = Mockito.mock(RestTemplate::class.java)
+
+        val httpRequestEntity = HttpEntity("someString")
+
         val mockResponse = ResponseEntity(listOf("1", ""), HttpStatus.ACCEPTED)
         Mockito.`when`(restTemplate.exchange(Mockito.anyString(), Mockito.eq(HttpMethod.GET), Mockito.any(), ArgumentMatchers.any<ParameterizedTypeReference<List<String>>>())).thenReturn(mockResponse)
-        val exchange = restTemplate.exchange<List<String>>("/someUrl/{someVal}", HttpMethod.GET, null, "1")
+        val exchange = restTemplate.exchange<List<String>>("/someUrl/{someVal}", HttpMethod.GET, httpRequestEntity, "1")
         println(exchange)
     }
 
