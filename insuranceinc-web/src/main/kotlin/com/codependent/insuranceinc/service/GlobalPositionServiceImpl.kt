f36d218e961cc3603582f7e6db16c3d2809f36f3
Jose Antonio Iñigo Garrido <jainigo@profile.es>
2019-04-10 23:35:41 +0200

    logger

--- insuranceinc-web/src/main/kotlin/com/codependent/insuranceinc/service/GlobalPositionServiceImpl.kt
+++ insuranceinc-web/src/main/kotlin/com/codependent/insuranceinc/service/GlobalPositionServiceImpl.kt
@@ -1,6 +1,7 @@
 package com.codependent.insuranceinc.service
 
 import com.codependent.insuranceinc.configuration.InsuranceincConfigurationProperties
+import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
 import org.springframework.web.reactive.function.client.WebClient
 import reactor.core.publisher.Mono
@@ -10,8 +11,11 @@ import reactor.core.publisher.onErrorReturn
 class GlobalPositionServiceImpl(private val config: InsuranceincConfigurationProperties,
                                 private val webClient: WebClient) : GlobalPositionService {
 
-    override fun getGlobalPosition(userId: String): Mono<Map<*, *>> {
+    private val logger = LoggerFactory.getLogger(javaClass)
 
+
+    override fun getGlobalPosition(userId: String): Mono<Map<*, *>> {
+        logger.debug("getGlobalPosition({})", userId)
         return webClient.get().uri("${config.globalPositionUrl}/globalPosition/${userId}")
                 .retrieve().bodyToMono(Map::class.java)
                 .log()
--- insuranceinc-web/src/main/kotlin/com/codependent/insuranceinc/web/GlobalPositionController.kt
+++ insuranceinc-web/src/main/kotlin/com/codependent/insuranceinc/web/GlobalPositionController.kt
@@ -1,6 +1,7 @@
 package com.codependent.insuranceinc.web
 
 import com.codependent.insuranceinc.service.GlobalPositionService
+import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
@@ -9,10 +10,12 @@ import org.springframework.web.server.WebSession
 @Controller
 class GlobalPositionController(private val globalPositionService: GlobalPositionService) {
 
+    private val logger = LoggerFactory.getLogger(javaClass)
+
     @GetMapping("/globalPosition")
     fun globalPosition(webSession: WebSession, model: Model) {
+        logger.debug("getGlobalPosition")
         val globalPosition = globalPositionService.getGlobalPosition(webSession.attributes["userId"] as String)
-                .log()
         model.addAttribute("globalPosition", globalPosition)
     }
 
