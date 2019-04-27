f5b8f5309680362160f19387db17d26b02133673
Jose Antonio Iñigo Garrido <jainigo@profile.es>
2019-04-27 02:38:43 +0200

    mapped exception

--- customer-rating-service/src/main/kotlin/com/codependent/insuranceinc/customerrating/web/handler/CustomerRatingHandler.kt
+++ customer-rating-service/src/main/kotlin/com/codependent/insuranceinc/customerrating/web/handler/CustomerRatingHandler.kt
@@ -2,13 +2,16 @@ package com.codependent.insuranceinc.customerrating.web.handler
 
 import com.codependent.insuranceinc.customerrating.dto.CustomerRating
 import com.codependent.insuranceinc.customerrating.service.CustomerRatingsService
+import org.springframework.http.HttpStatus
 import org.springframework.stereotype.Component
 import org.springframework.web.reactive.function.server.ServerRequest
 import org.springframework.web.reactive.function.server.ServerResponse.ok
+import org.springframework.web.server.ResponseStatusException
 
 @Component
 class CustomerRatingHandler(private val customerRatingsService: CustomerRatingsService) {
 
     fun getPolicies(serverRequest: ServerRequest) =
             ok().body(customerRatingsService.getRating(serverRequest.pathVariable("userId")), CustomerRating::class.java)
+                    .onErrorMap { _ -> ResponseStatusException(HttpStatus.SERVICE_UNAVAILABLE) }
 }
